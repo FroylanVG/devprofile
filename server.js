34b0f6e7ad29d579ba252556b01a1f66dd83c84e
Froylan Villaverde Gonzalez <froy.villaverde@gmail.com>
2021-01-22 23:53:39 -0600

    Database connection added.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const { response } = require('express');
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
+
+//Connect DB
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'))
 
